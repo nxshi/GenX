7ddff57111fe07772bd02f6acbdb7f0f7163dc49
Qingyu Xu <qingyux@princeton.edu>
2022-05-05 15:21:09 -0400

    Update write_co2.jl
    so that dfCapturedEmissions_plant will always be defined.

--- src/write_outputs/write_co2.jl
+++ src/write_outputs/write_co2.jl
@@ -28,7 +28,7 @@ function write_co2(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
 
     # CO2 emissions by zone
 
-    dfEmissions = DataFrame(Zone = 1:Z, AnnualSum = zeros(Float64, Z))
+    dfEmissions = DataFrame(Zone=1:Z, AnnualSum=zeros(Float64, Z))
 
     emissions_zone = zeros(Z, T)
     emissions_zone = value.(EP[:eEmissionsByZone])
@@ -64,9 +64,9 @@ function write_co2(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
     dfEmissions_plant = vcat(dfEmissions_plant, total)
     CSV.write(joinpath(path, "emissions_plant.csv"), dftranspose(dfEmissions_plant, false), writeheader=false)
     
+    dfCapturedEmissions_plant = DataFrame(Resource=inputs["RESOURCES"], Zone=dfGen[!, :Zone], AnnualSum=zeros(G))
     if setup["CO2Capture"] == 1
         # Captured CO2 emissions by plant
-        dfCapturedEmissions_plant = DataFrame(Resource=inputs["RESOURCES"], Zone=dfGen[!, :Zone], AnnualSum=zeros(G))
         emissions_captured_plant = zeros(G, T)
         emissions_captured_plant = (value.(EP[:eEmissionsCaptureByPlant]))
         if setup["ParameterScale"] == 1
@@ -74,14 +74,14 @@ function write_co2(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
         end
         dfCapturedEmissions_plant.AnnualSum .= emissions_captured_plant * inputs["omega"]
         dfCapturedEmissions_plant = hcat(dfCapturedEmissions_plant, DataFrame(emissions_captured_plant, :auto))
-    
+
         auxNew_Names = [Symbol("Resource"); Symbol("Zone"); Symbol("AnnualSum"); [Symbol("t$t") for t = 1:T]]
         rename!(dfCapturedEmissions_plant, auxNew_Names)
-    
+
         total = DataFrame(["Total" 0 sum(dfCapturedEmissions_plant[!, :AnnualSum]) fill(0.0, (1, T))], auxNew_Names)
         total[!, 4:T+3] .= sum(emissions_captured_plant, dims=1)
         dfCapturedEmissions_plant = vcat(dfCapturedEmissions_plant, total)
-    
+
         CSV.write(joinpath(path, "captured_emissions_plant.csv"), dftranspose(dfCapturedEmissions_plant, false), writeheader=false)
     end
 
